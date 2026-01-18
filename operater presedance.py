feb5dd231ec6e48cb258fcc3a4d43477d245c8a5
anwaraneeqa60-lang <anwaraneeqa60@gmail.com>
2026-01-17 20:53:32 -0500

    first commit

---
+++ operater presedance.py
@@ -0,0 +1,15 @@
+c = 6
+v = 8
+n = 10
+h = 4
+z = 12
+z= (c+v) * c / h 
+print("value of (c+v) * c / h", z)
+
+name = "alex"
+age = 0
+if name == "alex" or name == "ayaan" and age <= 12:
+    print("hello welcome!!")
+else:
+    print("good bye!!")
+
