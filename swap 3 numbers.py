ce32e01272eb9a467d774ef2472490a2754c42fa
anwaraneeqa60-lang <anwaraneeqa60@gmail.com>
2026-01-19 20:07:58 -0500

    swap 3 numbers.py

--- swap 3 numbers.py
+++ swap 3 numbers.py
@@ -1,8 +1,8 @@
 num1 =  32
 num2 =  29
 num3 =  13
-print(F"normal numbers:num1 = {num1}, num2 = {num2}, num3 = {num3}")
+print(F"normal numbers: num1 = {num1} num2 = {num2} num3 = {num3}")
 
 num1,  num2,  num3 =  swap_three_numbers  =  num1,  num2,  num3
 
-print(F"switched numbers:num1 = {num2}, num2 = {num3}, num3 = {num1}")
+print(F"switched numbers: num1 = {num2} num2 = {num3} num3 = {num1}")
