6e8eabf2f590c5b7f82775a5c880a736618bf6c5
anwaraneeqa60-lang <anwaraneeqa60@gmail.com>
2026-01-19 19:11:46 -0500

    swap 3 numbers .py

---
+++ divisble number.py
@@ -0,0 +1,6 @@
+print("enter a number"(numerater))
+numn = int(input())
+print("enter a number"(denominater))
+numd = int(input())
+if numn%numd ==0:
+    print("/n" + str (numn) "is divisble by"+ str (numd))
